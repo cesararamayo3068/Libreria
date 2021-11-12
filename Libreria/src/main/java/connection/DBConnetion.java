13390dc50d13faa1c8da3fc6be53bfa695997999
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-11-12 10:46:43 -0300

    Clase connection

---
+++ Libreria/src/main/java/connection/DBConnetion.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnetion {
+	static String bd = "libreria";
+	static String port = "3307";
+	static String usuario = "root";
+	static String password = "pass";
+	static String cadenaDeConexion = "jdbc:mysql://localhost:" + port + "/" + bd;
+
+	Connection connection = null;
+
+	public void DBConnection() {
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			connection = DriverManager.getConnection(cadenaDeConexion, usuario, password);
+
+			if (connection == null) {
+				System.out.println("la conexion ha fallado");
+
+			} else {
+				System.out.println("la conexion a" + bd + "ha sido satisfactoria");
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void descoenctar() {
+		connection = null;
+
+	}
+
+}
