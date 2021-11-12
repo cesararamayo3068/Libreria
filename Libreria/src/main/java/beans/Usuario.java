d193cde33baf0e14a343e8408bc19eec057dad6e
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-11-12 08:16:28 -0300

    getters y setters y toString

--- Libreria/src/main/java/beans/Usuario.java
+++ Libreria/src/main/java/beans/Usuario.java
@@ -20,6 +20,68 @@ public class Usuario {
 		this.saldo = saldo;
 		this.premium = premium;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public boolean isPremium() {
+		return premium;
+	}
+
+	public void setPremium(boolean premium) {
+		this.premium = premium;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [username=" + username + ", contrasena=" + contrasena + ", nombre=" + nombre + ", apellidos="
+				+ apellidos + ", email=" + email + ", saldo=" + saldo + ", premium=" + premium + "]";
+	}
 	
 	
 
