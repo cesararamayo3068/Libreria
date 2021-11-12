84fb298fc98e8c30eaec968b3ac8f32a97218577
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-11-12 11:40:02 -0300

    metodo Actualizar libros

--- Libreria/src/main/java/test/OperacionesBD.java
+++ Libreria/src/main/java/test/OperacionesBD.java
@@ -6,13 +6,24 @@ import java.sql.Statement;
 import connection.DBConnection;
 
 public class OperacionesBD {
-	
+
 	public static void main(String[] args) {
 		actualizarLibro(1, "Histórica");
 	}
 
 	public static void actualizarLibro(int id, String genero) {
 		DBConnection con = new DBConnection();
+		String sql = "UPDATE libros SET genero = '" + genero + "'WHERE id=" + id;
+
+		try {
+			Statement st = con.getConnection().createStatement();
+			st.executeQuery(sql);
+
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		} finally {
+			con.descoenctar();
+		}
 	}
 
 }
